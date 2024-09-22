4019bd4774fd07fe7678fa29f9913868354f33a3
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-09-22 16:49:55 +0300

    Add sudoku_board_checker_google.py File to Project Folder -- 1st Commit

--- hash_table_anagrams.py
+++ hash_table_anagrams.py
@@ -1,6 +1,8 @@
+import time
+
 # Description: Given an array of strings, group anagrams together.
 # Cool solution, but only if the characters don't repeat in the strings.
-def groupAnagrams(strs):
+def groupAnagramsUnique(strs):
     """
     :type strs: List[str]
     :rtype: List[List[str]]
@@ -17,3 +19,44 @@ def groupAnagrams(strs):
     for i in anagramMap:
         result.append(anagramMap[i])
     return result
+
+# Declare variables for groupAnagramsUnique function
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Time the function
+
+start_time = time.time()
+
+print(groupAnagramsUnique(strs))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# Description: Given an array of strings, group anagrams together.
+# This is a better solution for when the characters repeat in the strings.
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    anagramMap = {}
+    for i in strs:
+        sortedWord = ''.join(sorted(i))
+        if sortedWord not in anagramMap:
+            anagramMap[sortedWord] = []
+        anagramMap[sortedWord].append(i)
+    result = []
+    for i in anagramMap:
+        result.append(anagramMap[i])
+    return result
+
+# Declare variables for groupAnagrams function
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Time the function
+
+start_time = time.time()
+
+print(groupAnagrams(strs))
+
+print("--- %s seconds ---" % (time.time() - start_time))
