91ea2065ba7801041710e29fba4616d04cd55768
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-09-30 15:14:46 +0300

    Add Version 2.0 to 4sum_google.py File -- 1st Commit

--- 4sum_google.py
+++ 4sum_google.py
@@ -7,13 +7,27 @@ def fourSumCount(nums1, nums2, nums3, nums4):
     :rtype: int
     """
     count = 0
-    for i in nums1:
-        for j in nums2:
-            for k in nums3:
-                nr = 0 - k - j - i
-                aux = []
-                aux.extend(nums4)
-                while nr in aux:
-                    count += 1
-                    aux.remove(nr)
+    ComboMap12 = {}
+    ComboMap34 = {}
+    for i in range(0, len(nums1)):
+        for j in range(0, len(nums1)):
+
+            key12 = nums1[i] + nums2[j]
+            key34 = nums3[i] + nums4[j]
+
+            if key12 not in ComboMap12:
+                ComboMap12[key12] = 1
+            else:
+                ComboMap12[key12] += 1
+
+            if key34 not in ComboMap34:
+                ComboMap34[key34] = 1
+            else:
+                ComboMap34[key34] += 1
+
+    for i in ComboMap12:
+        opposite = i * (-1)
+        if opposite in ComboMap34:
+            count += ComboMap12[i] * ComboMap34[opposite]
+
     return count
